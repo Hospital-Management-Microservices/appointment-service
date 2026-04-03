107eda9e8a356b9f41c9f9b4d8d908d39ac86b31
Kalpa Vidusha Pathirana <kalpa.vidusha@gmail.com>
2026-04-03 21:49:24 +0530

    Simplify OpenAPI info in SwaggerConfig
    Shorten the OpenAPI info by trimming the long description

--- src/main/java/com/hms/appointment/config/SwaggerConfig.java
+++ src/main/java/com/hms/appointment/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.hms.appointment.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,12 +17,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("HMS - Appointment Service API")
-                        .description("Hospital Management System - Appointment Service\n\n" +
-                                "Manages all patient appointments with doctors across departments.\n\n" +
-                                "**Status values:** SCHEDULED | COMPLETED | CANCELLED | NO_SHOW")
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("HMS Team - Member 4")
-                                .email("member4@hms.com")));
+                        .description("Manages all patient appointments with doctors across departments.")
+                        .version("1.0.0"));
     }
 }
